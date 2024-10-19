8464a736782c320f94f80d3ef3cb9fdc9e5beca7
ayush-270 <ayush27.work@gmail.com>
2024-10-19 21:43:25 +0530

    Added Speech Recognition

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,6 @@
 import pyttsx3
+import speech_recognition as sr
+import pyaudio as audio
 
 
 # Creating the function to make the assistant speak
@@ -22,4 +24,30 @@ def speak(text):
     engine.runAndWait()
 
 # checking the function.
-speak("Hey there this is you cortix ai")
+# speak("Hey there this is you cortix ai")
+
+# Creating the function for speech recognition.
+
+def TakeCommand():
+    # initializing the recognizer
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source :
+        print("Listening....")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source , 10 , 8)
+
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said : {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text = TakeCommand()
+
+speak(text)
