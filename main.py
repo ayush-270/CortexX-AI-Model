65ae37bc10dc0bd1640ea6cc6ba039b4eff79126
ayush-270 <ayush27.work@gmail.com>
2024-11-21 10:35:41 +0530

    Face Authentication Added

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import eel
-
 from engine.features import *
 from engine.command import *
+from engine.auth import recognise
 
 def start () :
     eel.init("www")
@@ -10,9 +10,23 @@ def start () :
     ''' Creating app mode for the AI
     Initialiasing MS browser to open our index.html file. '''
 
-
     playAssistantSound()
+    @eel.expose
+    def init():
+        # subprocess.call([r'device.bat'])
+        eel.hideLoader()
+        speak("Ready for face authentication")
+        flag = recognise.AuthenticateFace()
+        if flag == 1:
+            eel.hideFaceAuth()
+            speak("Face Authentication Successfull")
+            eel.hideFaceAuthSuccess()
+            speak("Hello, Welcome The The CortexX AI Model.")
+            eel.hideStart()
+            playAssistantSound()
+        else:
+            speak("Face Authentication Failed.")
 
-    os.system('start Chrome.exe --app = "http://localhost:8000/index.html"')
+    #os.system('start Chrome.exe --app = "http://localhost:8000/index.html"')
 
     eel.start("index.html", block = True) 
